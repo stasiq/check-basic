0b9c0d14e4293a20f20b8b95146235354a3d0fde
s.konovalov <s.konovalov@intensa.ru>
2023-05-04 17:12:36 +0300

    Удаление комментариев

--- index.php
+++ index.php
@@ -8,15 +8,3 @@ use App\Check as Check;
 
 $check = new Check();
 $check->Check();
-
-/*
-$url = 'http://comagic2.intensa-dev.ru/robots.txt';
-$url = 'https://www.comagic.ru/robots.txt';
-$headers = get_headers($url, 1);
-
-if (isset($headers['WWW-Authenticate'])) {
-    echo 'Basic-y';
-} else {
-    $contents = file_get_contents($url);
-    echo '<pre>',print_r($contents),'</pre>'; ;
-}*/
